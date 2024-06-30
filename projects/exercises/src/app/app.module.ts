34443467da3d54cf00d83530efa198868891bd75
Diegokraus <diegokraus@gmail.com>
2024-06-30 00:59:21 -0300

    feat: adicionado controle de pagamento

---
+++ projects/exercises/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import ptBr from '@angular/common/locales/pt';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
+import { PaymentWaysComponent } from './payment-ways/payment-ways.component';
+import { CreditCardComponent } from './credit-card/credit-card.component';
+
+registerLocaleData(ptBr);
+
+@NgModule({
+    declarations: [AppComponent, PaymentComponent, PaymentWaysComponent, CreditCardComponent],
+    imports: [BrowserModule, FormsModule],
+    providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
